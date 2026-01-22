03bd10ab2bdf3187a37e4302daa525fc80da827d
Zuolong Wu <zuolong2187@gmail.com>
2026-01-22 14:02:32 -0500

    Added element to todos

--- falkordb/parse.py
+++ falkordb/parse.py
@@ -1,12 +1,28 @@
 import json
 
-from model_tool import Tool
+from src.categorization.keyword_taxonomy import (
+    get_all_categories,
+    get_all_keywords,
+)
+from src.models import Tool
 
-with open("tools.json") as f:
-    tools = json.load(f)["tools"]
-for tool in tools:
-    add_to_falkordb(tool)
 
-
-def add_to_falkordb(tool):
+def add_to_falkordb(tool_dict):
     """Add tool to graph."""
+    tool: Tool = Tool.model_validate(tool_dict)
+
+
+def add_tools():
+    with open("tools.json") as f:
+        tools = json.load(f)["tools"]
+    for tool in tools:
+        add_to_falkordb(tool)
+
+
+def main():
+    print(get_all_keywords())
+    print(get_all_categories())
+
+
+if __name__ == "__main__":
+    main()
